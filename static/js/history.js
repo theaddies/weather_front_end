aa13339444164cd90294c3dc9bd5dc6b6ede9bdd
Andrew Addie <theaddies@neo.rr.com>
2022-03-18 06:35:57 -0400

    added sort routing that lists data by id and makes graphs smooth

--- static/js/history.js
+++ static/js/history.js
@@ -5,6 +5,8 @@ const url = "https://weatherapi.smartnprivate.com/posts/day";
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
 
+data.sort(function(a, b) {return a.id - b.id});
+
 var tempValues = data.map(d => d.temp);
 var humidValues = data.map(d => d.humid);
 var wind_directionValues = data.map(d => d.wind_direction);
@@ -65,8 +67,9 @@ var graphData2 = [{
       var humidValues = data.map(d => d.humid);
     console.log('humid values');
     console.log(humidValues)
-    
+    console.log('object values');
       console.log(Object.values(data))
+      console.log('object keys');
       console.log(Object.keys(data))
     var list = [data]
     console.log('simple for each');
@@ -80,13 +83,25 @@ var graphData2 = [{
     
      d3.select(".col-md-12").select("thead").selectAll("tr").data([1]).enter().append("tr")
      .selectAll("th").data(Object.keys(data[0])).enter().append("th").text(d => d)
+
+    //var header = "<th>id</th><th>temp</th><th>humid</th><th>press</th><th>wind_speed</th><th>direction</th><th>voltage</th><th>current</th><th>power</th><th>created_at</th>"
     
+//     var header = "<th>id</th>"
+//     var header1 = "<th>temp</th>"
+
+//     var heading = d3.select(".col-md-12").select("thead").selectAll("tr").data([1]).enter().append("tr")
+//     .selectAll("th").data(Object.keys([1])).enter().append("th").html(header).html(header1);
+
+// //console.log(data[0]);
+
+// //console.log(Object.keys(data[0])[0]);
+
     var rows = d3.select("tbody").selectAll("tr").data(data).enter().append("tr");
     
     var cells = rows
     .html(function(d) {
-      return `<td>${d.humid}</td><td>${d.id}</td><td>${d.wind_direction}</td><td>${d.current}</td><td>${d.power}</td>
-      <td>${d.press}</td><td>${d.wind_speed}</td><td>${d.temp}</td><td>${d.bno_direction}</td><td>${d.voltage}</td><td>${d.created_at}</td>`
+      return `<td>${d.humid}</td><td>${d.temp}</td><td>${d.wind_speed}</td><td>${d.bno_direction}</td><td>${d.voltage}</td>
+      <td>${d.created_at}</td><td>${d.id}</td><td>${d.press}</td><td>${d.wind_direction}</td><td>${d.current}</td><td>${d.power}</td>`
     })
 
 });
